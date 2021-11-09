b101c13f9d7f57ed2030e53c390e0c64bb8391c7
Alex <alexp0beda88@gmail.com>
2021-11-09 21:54:23 +0300

    Убрал ActiveEngineMap, заинжектил engine в MAP

--- src/main/java/ru/pobeda/EngineMapConfig.java
+++ src/main/java/ru/pobeda/EngineMapConfig.java
@@ -1,18 +1,29 @@
 package ru.pobeda;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
-@Configuration
+@Service
+@Getter
 public class EngineMapConfig {
+
+
+    @Autowired
+    PetrolEngineImpl petrolEngine;
+
+    @Autowired
+    DieselEngineImpl dieselEngine;
 
     @Bean
     public HashMap<String, Engine> actionEngineMap() {
         HashMap<String, Engine> engineMap = new HashMap<>();
-        engineMap.put("petrol", new PetrolEngineImpl());
-        engineMap.put("diesel", new DieselEngineImpl());
+        engineMap.put("petrol", petrolEngine);
+        engineMap.put("diesel", dieselEngine);
         return engineMap;
     }
 }
